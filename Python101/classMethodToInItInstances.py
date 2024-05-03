484639a0bd5d69d3f2def91e89dcf124edb18baf
Rishi Raj <rishiraj88+github@gmail.com>
2024-05-03 16:50:37 +0200

    Class Inheritance demonstration.

--- Python101/classMethodToInItInstances.py
+++ Python101/classMethodToInItInstances.py
@@ -1,8 +1,9 @@
 class Employee:
     nEmployees = 0
     increment = 0.01
-    def __init__(self,name,sal,increment) -> None:
-        self.id=name
+
+    def __init__(self, name, sal, increment) -> None:
+        self.id = name
         self.name = name
         self.sal = sal
         self.increment = increment
@@ -10,10 +11,11 @@ class Employee:
 
     def raiseMySalaryWithMyProfileRate(self):
         self.sal += self.sal * self.increment
+
     @classmethod
-    def from_str(cls,csv_data_str):
-        name,sal,inc = csv_data_str.split(",")
-        return cls(name,float(sal),float(inc))
+    def from_str(cls, csv_data_str):
+        name, sal, inc = csv_data_str.split(",")
+        return cls(name, float(sal), float(inc))
 
 
 ronak = Employee.from_str("name003,30000,.2")
