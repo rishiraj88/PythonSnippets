486d57422505fe2a686839ee77a3e27a2abff849
Rishi Raj <rishiraj88+github@gmail.com>
2024-05-03 10:26:26 +0200

    Add class methods and instance methods.

--- Python101/objectMembers.py
+++ Python101/objectMembers.py
@@ -1,11 +1,27 @@
 class Employee:
     nEmployees = 0
+    increment = 0.01
     def __init__(self,name,sal,increment) -> None:
+        self.id=name
         self.name = name
         self.sal = sal
         self.increment = increment
         Employee.nEmployees += 1
 
+    def raiseMySalaryAtStandardRate(self):
+        self.sal = self.sal*(1+Employee.increment)
+    
+    def raiseMySalaryAtMyProfileRate(self):
+        self.sal = self.sal*(1+self.increment)
+
+    def raiseMySalaryAtArbitraryRate(self,incRate):
+        self.sal = self.sal*(1+incRate)
+
+    @classmethod
+    def modifyIncrementRate(cls,newRate):
+        increment = newRate
+
+# 1. to view the data members of instances and of class 
 print(Employee.nEmployees)
 emp001 = Employee("name01",20000,0.02)
 print(Employee.nEmployees)
@@ -18,3 +34,14 @@ print(emp001.__dict__)
 print(emp001.__class__)
 print("---------")
 print(Employee.__dict__)
+
+# 2. to use instance method and class method
+print(emp001.sal)
+emp001.raiseMySalaryAtMyProfileRate()
+print(emp001.sal)
+emp001.sal = 20000
+emp001.raiseMySalaryAtStandardRate()
+print(emp001.sal)
+emp001.sal = 20000
+emp001.raiseMySalaryAtArbitraryRate(.3) # raise by 30 percent
+print(emp001.sal)
