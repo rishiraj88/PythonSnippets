4ffc0824bee1d4b9e55363202b0436db075a8410
Rishi Raj <rishiraj88+github@gmail.com>
2025-04-29 11:30:16 +0200

    Implement a function for Armstrong numbers.

--- Python101/armstrongNumbersInRange.py
+++ Python101/armstrongNumbersInRange.py
@@ -1,17 +1,22 @@
 print("To check which integers in a both-limits-inclusive range are Armstrong numbers")
-low =int(input("Enter lower limit of the range:"))
-high=int(input("Enter lower limit:"))
+low =int(input("Enter the lower limit of the range: "))
+high=int(input("Enter the upper limit: "))
 
-#assuming both numbers to be in a common order
-armstrongNumbers = []
-for n in range(low,high+1):
-    sum=0
-    orderLengthOfNumber = len(str(n))
-    temp = n
-    while 0 != temp:        
-        digit = temp % 10
-        sum += digit ** orderLengthOfNumber
-        temp //= 10
-    if n == sum:
-        armstrongNumbers.append(n)
-print("\nArmstrong numbers in this range are: ",armstrongNumbers)
+#assuming both numbers to be in a common order. Order means the count of digits in a given number
+def armstrongNumsInRange(low,high):
+    armstrongNumbers = []
+    for n in range(low, high + 1):
+        sum = 0
+        orderLengthOfNumber = len(str(n))
+        temp = n
+        while 0 != temp:
+            digit = temp % 10
+            sum += digit ** orderLengthOfNumber
+            temp //= 10
+        if n == sum:
+            armstrongNumbers.append(n)
+    print(f"\nArmstrong numbers in the range [{low},{high}] are: {armstrongNumbers}")
+    return armstrongNumbers
+
+if __name__=='__main__':
+    armstrongNumsInRange(111,999)
